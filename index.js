0f70a6bdc9ae2559a655e3f6c6968ae59dc9848a
unknown <alimovf21@gmail.com>
2022-02-22 17:34:02 +0500

    fara

---
+++ index.js
@@ -0,0 +1,20 @@
+// Create a Set
+const letters = new Set();
+
+// Add Values to the Set
+letters.add("a");
+letters.add("b");
+letters.add("c");
+
+
+console.log(letters);
+
+// Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+
+console.log(fruits);
